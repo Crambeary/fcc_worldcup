299f4328875b7b0b4285f5586db59b2956c8544b
Crambeary <marctotaro94@gmail.com>
2024-10-16 14:56:12 +0000

    removed use of winner and opponent column in games table

--- insert_data.sh
+++ insert_data.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-# if [[ $1 == "test" ]]; then
-#   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-# else
-#   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-# fi
+if [[ $1 == "test" ]]; then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
-PSQL="psql --username=marct --dbname=worldcup -t --no-align -c"
+# PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 echo $($PSQL "TRUNCATE games, teams")
@@ -39,7 +39,7 @@ do
   then
     WINNER_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name LIKE '$WINNER'")
     OPPONENT_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name LIKE '$OPPONENT'")
-    INSERT_VALUE_RESULT=$($PSQL "INSERT INTO games(year, round, winner, opponent, winner_goals, opponent_goals, winner_id, opponent_id) VALUES($YEAR, '$ROUND', '$WINNER', '$OPPONENT', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_TEAM_ID, $OPPONENT_TEAM_ID);")
+    INSERT_VALUE_RESULT=$($PSQL "INSERT INTO games(year, round, winner_goals, opponent_goals, winner_id, opponent_id) VALUES($YEAR, '$ROUND', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_TEAM_ID, $OPPONENT_TEAM_ID);")
     if [[ $INSERT_VALUE_RESULT == "INSERT 0 1" ]]; then
       echo Inserted into games $YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS
     fi
